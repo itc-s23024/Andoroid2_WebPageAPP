0f545648aa3ca2f3a65a86d01f663dbf03ba1c11
itc-s23024 <s23024@std.it-college.ac.jp>
2024-10-30 14:27:07 +0900

    完成

--- app/src/main/java/jp/ac/it_college/std/s23024/webpageapp/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s23024/webpageapp/MainActivity.kt
@@ -5,13 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import jp.ac.it_college.std.s23024.webpageapp.ui.SecondaryTabRowScreen
 import jp.ac.it_college.std.s23024.webpageapp.ui.theme.WebPageAppTheme
+import jp.ac.it_college.std.s23024.webpageapp.ui.WebView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +17,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WebPageAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SecondaryTabRowScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WebPageAppTheme {
-        Greeting("Android")
     }
 }
